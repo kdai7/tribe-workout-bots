11eea4de82a10976e7729129fe006de2ff74c9d3
kdai7 <35241631+kdai7@users.noreply.github.com>
2020-05-19 13:47:21 -0400

    update first post num

--- wreck_db.py
+++ wreck_db.py
@@ -28,7 +28,7 @@ def add_num_posts(mention_id, event_time, name, channel_id):
             "UPDATE wreck_data SET num_posts=num_posts+1 WHERE slack_id = %s"),
             [mention_id[0]])
         if cursor.rowcount == 0 and channel_id == "C013LDTN13Q":    #comment: add channel id here
-            cursor.execute(sql.SQL("INSERT INTO wreck_data VALUES (%s, 0, 0, 0, 0, 0, 0, now(), %s, %s)"),
+            cursor.execute(sql.SQL("INSERT INTO wreck_data VALUES (%s, 1, 0, 0, 0, 0, 0, now(), %s, %s)"),
                            [name, mention_id[0], event_time])
             send_debug_message("%s is new to Wreck" % name)
         conn.commit()
