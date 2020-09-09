ba8e2d58b4f511a303bb98345b48b1b2bfa66e48
kdai7 <35241631+kdai7@users.noreply.github.com>
2020-09-08 23:34:43 -0400

    team leaderboard

--- wreck_challenge.py
+++ wreck_challenge.py
@@ -209,6 +209,10 @@ class WreckA:
                 count += 1
                 to_print = collect_stats(3, True)
                 send_message(to_print, channel=self._channel, bot_name=self._name, url=self._avatar_url)
+            if "!teamleaderboard" in self._lower_text: #for team olympics
+                count += 1
+                to_print = "team leaderboard"
+                send_message(to_print, channel=self._channel, bot_name=self._name, url=self._avatar_url)
             if '!workouts' in self._lower_text:  # display the leaderboard for who works out the most
                 count += 1
                 to_print = collect_stats(2, True)
