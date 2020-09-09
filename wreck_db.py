87a6059d80bef76bdbf11bc6142d95124908ba34
kdai7 <35241631+kdai7@users.noreply.github.com>
2020-09-09 00:19:49 -0400

    debug

--- wreck_db.py
+++ wreck_db.py
@@ -94,7 +94,7 @@ def collect_team_stats(datafield, rev):  #for team olympics
             count += float(leaderboard[x][6])
         cursor.close()
         conn.close()
-        string1 += '%.1f',count
+        string1 += '%.1f'% (count)
         return string1
     except (Exception, psycopg2.DatabaseError) as error:
         send_debug_message(error)
